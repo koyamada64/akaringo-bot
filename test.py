a65a9708927df734ed2830d8639c374c35302179
koyamada64 <koyamada64@gmail.com>
2020-09-18 03:20:00 +0900

    a

---
+++ test.py
@@ -0,0 +1,8 @@
+﻿# -*- coding: utf-8 -*-
+import os
+import TextTweet
+
+# APIの秘密鍵
+CK,CS,AT,ATS=os.environ["CK"], os.environ["CS"], os.environ["AT"], os.environ["AS"]
+
+TextTweet.puttweet(CK,CS,AT,AS)
