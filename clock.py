ede0c4d81ac1cf77e15604ecf3d62bc84e53f995
koyamada64 <koyamada64@gmail.com>
2020-09-19 01:18:53 +0900

    ランダムにツイートする（３０分毎）

--- clock.py
+++ clock.py
@@ -2,6 +2,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 import TextTweet
+from PrepareChain import PrepareChain
 
 # APIの秘密鍵
 CK,CS,AT,AS=os.environ["CK"], os.environ["CS"], os.environ["AT"], os.environ["AS"]
@@ -9,8 +10,14 @@ CK,CS,AT,AS=os.environ["CK"], os.environ["CS"], os.environ["AT"], os.environ["AS
 twische = BlockingScheduler()
 
 # 30分に一度ツイート
-@twische.scheduled_job('interval',minutes=15)
+@twische.scheduled_job('interval',minutes=30)
 def timed_job():
+    f = open("data.txt",encoding="utf-8")
+    text = f.read()
+    f.close()
+    chain = PrepareChain(text)
+    triplet_freqs = chain.make_triplet_freqs()
+    chain.save(triplet_freqs, True)
     TextTweet.puttweet(CK,CS,AT,AS)
 
 if __name__ == "__main__":
--- test.py
+++
@@ -1,8 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-import os
-import TextTweet
-
-# APIの秘密鍵
-CK,CS,AT,AS=os.environ["CK"], os.environ["CS"], os.environ["AT"], os.environ["AS"]
-
-TextTweet.puttweet(CK,CS,AT,AS)
